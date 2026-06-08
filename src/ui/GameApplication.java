ee1e2e978d292e90bb464f3bb3273fdc3b6c7534
Chrollo <nsg6794@gmail.com>
2026-06-08 16:47:53 +0100

    MainMenu added

--- src/ui/GameApplication.java
+++ src/ui/GameApplication.java
@@ -2,23 +2,33 @@ package ui;
 
 import game.GameCharacter;
 import game.Main;
+
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class GameApplication extends Application {
-    private static final int WINDOW_WIDTH = 1600;
-    private static final int WINDOW_HEIGHT = 1200;
+    private static final int WINDOW_WIDTH = 800;
+    private static final int WINDOW_HEIGHT = 600;
+
 
     @Override
     public void start(Stage stage) {
+        MainMenu mainMenu = new MainMenu();
+
+        stage.setTitle("Turn Based Game");
+        stage.setScene(mainMenu.createMenuScene(stage, this));
+        stage.show();
+    }
+
+    public void startGame(Stage stage) {
         GameCharacter player = Main.createPlayer();
         GameCharacter enemy = Main.createEnemy();
 
         CombatScreen combatScreen = new CombatScreen(player, enemy);
         Scene scene = new Scene(combatScreen, WINDOW_WIDTH, WINDOW_HEIGHT);
-
-        stage.setTitle("Roguelike");
+        
+        stage.setTitle("Game Name");
         stage.setScene(scene);
         stage.show();
     }
---
+++ src/ui/MainMenu.java
@@ -0,0 +1,36 @@
+package ui;
+
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.geometry.Pos;
+import javafx.stage.Stage;
+
+public class MainMenu {
+    public Scene createMenuScene(Stage stage, GameApplication gameApplication) {
+            
+        Button startButton = new Button("Start new Game");
+        Button loadButton = new Button("Load Game");
+        Button exitButton = new Button("Quit");
+
+        startButton.setOnAction(e -> {
+            gameApplication.startGame(stage);
+        });
+        
+        loadButton.setOnAction(e -> {
+            // Implement load game functionality
+        });
+        exitButton.setOnAction(e -> {
+            stage.close();  
+        });
+        VBox menuLayout = new VBox(20);
+        menuLayout.getChildren().addAll(startButton, loadButton, exitButton);
+        menuLayout.setAlignment(Pos.CENTER);
+        return new Scene(menuLayout, 400, 300);
+            
+            
+    
+           
+    }
+
+}
